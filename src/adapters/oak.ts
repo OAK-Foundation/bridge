523321ba9423eea9e86d3cd8f31c834c014712f3
chrisli30 <chrisli30@users.noreply.github.com>
2023-12-03 19:26:40 -0800

    Updated oak adapter

--- src/adapters/oak.ts
+++ src/adapters/oak.ts
@@ -15,113 +15,120 @@ import { createRouteConfigs, validateAddress } from "../utils";
 
 const DEST_WEIGHT = "5000000000";
 
-export const turingRouteConfigs = createRouteConfigs("turing", [
-	{
-		to: "karura",
-		token: "TUR",
-		xcm: {
-			fee: { token: "TUR", amount: "2560000000" },
-			weightLimit: DEST_WEIGHT,
-		},
-	},
-	{
-		to: "karura",
-		token: "KAR",
-		xcm: {
-			fee: { token: "KAR", amount: "6400000000" },
-			weightLimit: DEST_WEIGHT,
-		},
-	},
-	{
-		to: "karura",
-		token: "KUSD",
-		xcm: {
-			fee: { token: "KUSD", amount: "2626579278" },
-			weightLimit: DEST_WEIGHT,
-		},
-	},
-	{
-		to: "karura",
-		token: "LKSM",
-		xcm: {
-			fee: { token: "LKSM", amount: "480597195" },
-			weightLimit: DEST_WEIGHT,
-		},
-	},
-]);
-
 export const turingTokensConfig: Record<string, ExtendedToken> = {
   PHA: {
     name: "PHA",
     symbol: "PHA",
     decimals: 12,
     ed: "10000000000",
-    toRaw: () => "7",
+    toRaw: () => 7,
   },
   HKO: {
     name: "HKO",
     symbol: "HKO",
     decimals: 12,
     ed: "500000000000",
-    toRaw: () => "5",
+    toRaw: () => 5,
   },
   SDN: {
     name: "SDN",
     symbol: "SDN",
     decimals: 18,
     ed: "10000000000000000",
-    toRaw: () => "8",
+    toRaw: () => 8,
   },
   KSM: {
     name: "KSM",
     symbol: "KSM",
     decimals: 12,
     ed: "100000000",
-    toRaw: () => "1",
+    toRaw: () => 1,
   },
   MOVR: {
     name: "MOVR",
     symbol: "MOVR",
     decimals: 18,
     ed: "0",
-    toRaw: () => "9",
+    toRaw: () => 9,
   },
   AUSD: {
     name: "AUSD",
     symbol: "AUSD",
     decimals: 12,
     ed: "10000000000",
-    toRaw: () => "2",
+    toRaw: () => 2,
   },
   TUR: {
     name: "TUR",
     symbol: "TUR",
     decimals: 10,
     ed: "100000000",
-    toRaw: () => "0",
+    toRaw: () => 0,
   },
   SKSM: {
     name: "SKSM",
     symbol: "SKSM",
     decimals: 12,
     ed: "500000000",
-    toRaw: () => "6",
+    toRaw: () => 6,
   },
   KAR: {
     name: "KAR",
     symbol: "KAR",
     decimals: 12,
     ed: "100000000000",
-    toRaw: () => "3",
+    toRaw: () => 3,
   },
   LKSM: {
     name: "LKSM",
     symbol: "LKSM",
     decimals: 12,
     ed: "500000000",
-    toRaw: () => "4",
+    toRaw: () => 4,
+  },
+  MGX: {
+    name: "MGX",
+    symbol: "MGX",
+    decimals: 18,
+    ed: "0",
+    toRaw: () => -1,
   },
 };
+
+export const turingRouteConfigs = createRouteConfigs("turing", [
+  {
+    to: "moonriver",
+    token: "TUR",
+    xcm: {
+      fee: { token: "TUR", amount: "480597195" },
+      weightLimit: "Unlimited",
+    },
+  },
+  {
+    to: "moonriver",
+    token: "MOVR",
+    xcm: {
+      fee: { token: "MOVR", amount: "480597195" },
+      weightLimit: "Unlimited",
+    },
+  },
+  {
+    to: "shiden",
+    token: "TUR",
+    xcm: {
+      fee: { token: "TUR", amount: "480597195" },
+      weightLimit: "Unlimited",
+    },
+  },
+  {
+    to: "shiden",
+    token: "SDN",
+    xcm: {
+      fee: { token: "SDN", amount: "480597195" },
+      weightLimit: "Unlimited",
+    },
+  },
+]);
 
 export const turingStagingTokensConfig: Record<string, ExtendedToken> = {
   PHA: {
@@ -129,236 +136,255 @@ export const turingStagingTokensConfig: Record<string, ExtendedToken> = {
     symbol: "PHA",
     decimals: 12,
     ed: "10000000000",
-    toRaw: () => "7",
+    toRaw: () => 7,
   },
   HKO: {
     name: "HKO",
     symbol: "HKO",
     decimals: 12,
     ed: "500000000000",
-    toRaw: () => "5",
+    toRaw: () => 5,
   },
   SDN: {
     name: "SDN",
     symbol: "SDN",
     decimals: 18,
     ed: "10000000000000000",
-    toRaw: () => "8",
+    toRaw: () => 8,
   },
   KSM: {
     name: "KSM",
     symbol: "KSM",
     decimals: 12,
     ed: "100000000",
-    toRaw: () => "1",
+    toRaw: () => 1,
   },
   RSTR: {
     name: "RSTR",
     symbol: "RSTR",
     decimals: 18,
     ed: "1000000",
-    toRaw: () => "9",
+    toRaw: () => 9,
   },
   AUSD: {
     name: "AUSD",
     symbol: "AUSD",
     decimals: 12,
     ed: "10000000000",
-    toRaw: () => "2",
+    toRaw: () => 2,
   },
   TUR: {
     name: "TUR",
     symbol: "TUR",
     decimals: 10,
     ed: "100000000",
-    toRaw: () => "0",
+    toRaw: () => 0,
   },
   SKSM: {
     name: "SKSM",
     symbol: "SKSM",
     decimals: 12,
     ed: "500000000",
-    toRaw: () => "6",
+    toRaw: () => 6,
   },
   KAR: {
     name: "KAR",
     symbol: "KAR",
     decimals: 12,
     ed: "100000000000",
-    toRaw: () => "3",
+    toRaw: () => 3,
   },
   LKSM: {
     name: "LKSM",
     symbol: "LKSM",
     decimals: 12,
     ed: "500000000",
-    toRaw: () => "4",
+    toRaw: () => 4,
   },
 };
 
-export const turingDevTokensConfig: Record<string, ExtendedToken> = {
+export const turingStagingRouteConfigs = createRouteConfigs("turing-staging", [
+  {
+    to: "rocstar",
+    token: "TUR",
+    xcm: {
+      fee: { token: "TUR", amount: "2560000000" },
+      weightLimit: DEST_WEIGHT,
+    },
+  },
+]);
+
+export const turingLocalTokensConfig: Record<string, ExtendedToken> = {
   TUR: {
     name: "TUR",
     symbol: "TUR",
     decimals: 10,
     ed: "100000000",
-    toRaw: () => "0",
+    toRaw: () => 0,
   },
   SBY: {
     name: "SBY",
     symbol: "SBY",
     decimals: 18,
     ed: "10000000000000000",
-    toRaw: () => "4",
+    toRaw: () => 4,
   },
   DEV: {
     name: "DEV",
     symbol: "DEV",
     decimals: 18,
     ed: "1",
-    toRaw: () => "5",
+    toRaw: () => 5,
   },
   MGR: {
     name: "MGR",
     symbol: "MGR",
     decimals: 18,
     ed: "0",
-    toRaw: () => "1",
+    toRaw: () => 1,
   },
 };
 
+export const turingLocalRouteConfigs = createRouteConfigs("turing-local", [
+  {
+    to: "shibuya",
+    token: "TUR",
+    xcm: {
+      fee: { token: "TUR", amount: "2560000000" },
+      weightLimit: "Unlimited",
+    },
+  },
+]);
+
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const createBalanceStorages = (api: AnyApi) => {
-	return {
-		balances: (address: string) =>
-			Storage.create<DeriveBalancesAll>({
-				api,
-				path: "derive.balances.all",
-				params: [address],
-			}),
-		assets: (address: string, token: string) =>
-			Storage.create<any>({
-				api,
-				path: "query.tokens.accounts",
-				params: [address, token],
-			}),
-	};
+  return {
+    balances: (address: string) =>
+      Storage.create<DeriveBalancesAll>({
+        api,
+        path: "derive.balances.all",
+        params: [address],
+      }),
+    assets: (address: string, token: string) =>
+      Storage.create<any>({
+        api,
+        path: "query.tokens.accounts",
+        params: [address, token],
+      }),
+  };
 };
 
 class OakBalanceAdapter extends BalanceAdapter {
-	private storages: ReturnType<typeof createBalanceStorages>;
+  private storages: ReturnType<typeof createBalanceStorages>;
 
-	constructor({ api, chain, tokens }: BalanceAdapterConfigs) {
-		super({ api, chain, tokens });
-		this.storages = createBalanceStorages(api);
-	}
+  constructor({ api, chain, tokens }: BalanceAdapterConfigs) {
+    super({ api, chain, tokens });
+    this.storages = createBalanceStorages(api);
+  }
 
-	public subscribeBalance(token: string, address: string): Observable<BalanceData> {
-		if (!validateAddress(address)) throw new InvalidAddress(address);
+  public subscribeBalance(token: string, address: string): Observable<BalanceData> {
+    if (!validateAddress(address)) throw new InvalidAddress(address);
 
-		const storage = this.storages.balances(address);
+    const storage = this.storages.balances(address);
 
-		if (token === this.nativeToken) {
-			return storage.observable.pipe(
-				map((data) => ({
-					free: FN.fromInner(data.freeBalance.toString(), this.decimals),
-					locked: FN.fromInner(data.lockedBalance.toString(), this.decimals),
-					reserved: FN.fromInner(data.reservedBalance.toString(), this.decimals),
-					available: FN.fromInner(data.availableBalance.toString(), this.decimals),
-				})),
-			);
-		}
+    if (token === this.nativeToken) {
+      return storage.observable.pipe(
+        map((data) => ({
+          free: FN.fromInner(data.freeBalance.toString(), this.decimals),
+          locked: FN.fromInner(data.lockedBalance.toString(), this.decimals),
+          reserved: FN.fromInner(data.reservedBalance.toString(), this.decimals),
+          available: FN.fromInner(data.availableBalance.toString(), this.decimals),
+        })),
+      );
+    }
 
-		const tokenData: ExtendedToken = this.getToken(token);
+    const tokenData: ExtendedToken = this.getToken(token);
 
-		if (!tokenData) throw new TokenNotFound(token);
+    if (!tokenData) throw new TokenNotFound(token);
 
-		return this.storages.assets(address, tokenData.toRaw()).observable.pipe(
-			map((balance) => {
-				const amount = FN.fromInner(balance.free?.toString() || "0", tokenData.toRaw());
+    return this.storages.assets(address, tokenData.toRaw()).observable.pipe(
+      map((balance) => {
+        const amount = FN.fromInner(balance.free?.toString() || "0", tokenData.toRaw());
 
-				return {
-					free: amount,
-					locked: new FN(0),
-					reserved: new FN(0),
-					available: amount,
-				};
-			}),
-		);
-	}
-}
-
-class BaseOakAdapter extends BaseCrossChainAdapter {
-	private balanceAdapter?: OakBalanceAdapter;
-
-	public async init(api: AnyApi) {
-		this.api = api;
-
-		await api.isReady;
-
-		this.balanceAdapter = new OakBalanceAdapter({
-			chain: this.chain.id as ChainId,
-			api,
-			tokens: turingTokensConfig,
-		});
-	}
-
-	public subscribeTokenBalance(token: string, address: string): Observable<BalanceData> {
-		if (!this.balanceAdapter) {
-			throw new ApiNotFound(this.chain.id);
-		}
-
-		return this.balanceAdapter.subscribeBalance(token, address);
-	}
-
-	public subscribeMaxInput(token: string, address: string, to: ChainId): Observable<FN> {
-		if (!this.balanceAdapter) {
-			throw new ApiNotFound(this.chain.id);
-		}
-
-		return combineLatest({
-			txFee:
-				token === this.balanceAdapter?.nativeToken
-					? this.estimateTxFee({
-							amount: FN.ZERO,
-							to,
-							token,
-							address,
-							signer: address,
-					  })
-					: "0",
-			balance: this.balanceAdapter.subscribeBalance(token, address).pipe(map((i) => i.available)),
-		}).pipe(
-			map(({ balance, txFee }) => {
-				const tokenMeta = this.balanceAdapter?.getToken(token);
-				const feeFactor = 1.2;
-				const fee = FN.fromInner(txFee, tokenMeta?.decimals).mul(new FN(feeFactor));
-
-				// always minus ed
-				return balance.minus(fee).minus(FN.fromInner(tokenMeta?.ed || "0", tokenMeta?.decimals));
-			}),
-		);
-	}
-
-	public createTx(params: TransferParams): SubmittableExtrinsic<"promise", ISubmittableResult> | SubmittableExtrinsic<"rxjs", ISubmittableResult> {
-		return this.createXTokensTx(params);
-	}
-}
-
-export class TuringAdapter extends BaseOakAdapter {
-	constructor() {
-		super(chains.turing, turingRouteConfigs, turingTokensConfig);
-	}
-}
-
-export class TuringStagingAdapter extends BaseOakAdapter {
-  constructor() {
-    super(chains.turing, turingRouteConfigs, turingStagingTokensConfig);
+        return {
+          free: amount,
+          locked: new FN(0),
+          reserved: new FN(0),
+          available: amount,
+        };
+      }),
+    );
   }
 }
 
-export class TuringDevAdapter extends BaseOakAdapter {
+class BaseOakAdapter extends BaseCrossChainAdapter {
+  private balanceAdapter?: OakBalanceAdapter;
+
+  public async init(api: AnyApi) {
+    this.api = api;
+
+    await api.isReady;
+
+    this.balanceAdapter = new OakBalanceAdapter({
+      chain: this.chain.id as ChainId,
+      api,
+      tokens: this.tokens,
+    });
+  }
+
+  public subscribeTokenBalance(token: string, address: string): Observable<BalanceData> {
+    if (!this.balanceAdapter) {
+      throw new ApiNotFound(this.chain.id);
+    }
+
+    return this.balanceAdapter.subscribeBalance(token, address);
+  }
+
+  public subscribeMaxInput(token: string, address: string, to: ChainId): Observable<FN> {
+    if (!this.balanceAdapter) {
+      throw new ApiNotFound(this.chain.id);
+    }
+
+    return combineLatest({
+      txFee:
+        token === this.balanceAdapter?.nativeToken
+          ? this.estimateTxFee({
+              amount: FN.ZERO,
+              to,
+              token,
+              address,
+              signer: address,
+            })
+          : "0",
+      balance: this.balanceAdapter.subscribeBalance(token, address).pipe(map((i) => i.available)),
+    }).pipe(
+      map(({ balance, txFee }) => {
+        const tokenMeta = this.balanceAdapter?.getToken(token);
+        const feeFactor = 1.2;
+        const fee = FN.fromInner(txFee, tokenMeta?.decimals).mul(new FN(feeFactor));
+
+        // always minus ed
+        return balance.minus(fee).minus(FN.fromInner(tokenMeta?.ed || "0", tokenMeta?.decimals));
+      }),
+    );
+  }
+
+  public createTx(params: TransferParams): SubmittableExtrinsic<"promise", ISubmittableResult> | SubmittableExtrinsic<"rxjs", ISubmittableResult> {
+    return this.createXTokensTx(params);
+  }
+}
+export class TuringAdapter extends BaseOakAdapter {
   constructor() {
-    super(chains.turing, turingRouteConfigs, turingDevTokensConfig);
+    super(chains.turing, turingRouteConfigs, turingTokensConfig);
+  }
+}
+export class TuringStagingAdapter extends BaseOakAdapter {
+  constructor() {
+    super(chains["turing-staging"], turingStagingRouteConfigs, turingStagingTokensConfig);
+  }
+}
+export class TuringLocalAdapter extends BaseOakAdapter {
+  constructor() {
+    super(chains["turing-local"], turingLocalRouteConfigs, turingLocalTokensConfig);
   }
 }
